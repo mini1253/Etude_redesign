5e6cbcc63a7d492750d76606e2d3688d80e05545
mini1253 <tjalsgml7860@naver.com>
2023-04-24 00:18:46 +0900

    second commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let main = new Swiper(".main .swiper-container", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+
